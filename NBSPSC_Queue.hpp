5dbe589707a6365895843baee96a919bdc78286b
jeremyko <jeremyko69@gmail.com>
2015-12-19 18:40:40 +0900

    add files

---
+++ NBSPSC_Queue.hpp
@@ -0,0 +1,92 @@
+//Single Producer Single Consumer lock free queue using array
+//Non-blocking operation
+
+#include <atomic>
+
+//const int MAX_ARRAY_CNT = 1000;
+
+typedef enum __OPERATION_RESULT__
+{
+    QUEUE_OK  = 0,
+    QUEUE_FULL  ,
+    QUEUE_EMPTY
+
+} OPERATION_RESULT;
+
+template <typename T, int MAX_ARRAY_CNT>
+class ThreadSafeQueueSPSC
+{
+    private:
+        std::atomic<size_t> nWriteIndex_ ;
+        std::atomic<size_t> nReadIndex_ ;
+        T arrayOfData_[MAX_ARRAY_CNT];
+        const int ST_QUEUE_DATA_LEN = sizeof (T);
+
+    public:
+
+        ThreadSafeQueueSPSC()
+        {
+            nWriteIndex_ = 0;
+            nReadIndex_  = 0;
+        }
+
+        ///////////////////////////////////////////////////////////
+        OPERATION_RESULT Push(T* pQueuedata )
+        {
+            if ( nWriteIndex_  >= MAX_ARRAY_CNT )
+            {
+                if( nReadIndex_ == 0 )
+                {
+                    return QUEUE_FULL; 
+                }
+
+                nWriteIndex_ = 0 ;
+
+                return QUEUE_FULL; 
+            }
+            else if ( nWriteIndex_ < nReadIndex_ )
+            {
+                if ( nWriteIndex_ +1  >= nReadIndex_ )
+                {
+                    return QUEUE_FULL; 
+                }
+            }
+
+            memcpy( & arrayOfData_[nWriteIndex_], pQueuedata, ST_QUEUE_DATA_LEN);
+            
+            nWriteIndex_++  ;
+
+            return QUEUE_OK ;
+        }
+
+        ///////////////////////////////////////////////////////////
+        OPERATION_RESULT Pop(T** ppQueuedata)
+        {
+            if (nWriteIndex_ == nReadIndex_)
+            {
+                return QUEUE_EMPTY ;
+            }
+            else if ( nReadIndex_ >= MAX_ARRAY_CNT )
+            {
+                if( nWriteIndex_ == 0 )
+                {
+                    return QUEUE_EMPTY ; 
+                }
+                
+                nReadIndex_ = 0 ;
+            }
+
+            *ppQueuedata = & arrayOfData_[nReadIndex_];
+            
+            return QUEUE_OK ;
+        }
+
+        ///////////////////////////////////////////////////////////
+        void CommitPop()
+        {
+            nReadIndex_++ ;
+        }
+
+};
+
+
---
+++ NBSPSC_QueueTest.cpp
@@ -0,0 +1,105 @@
+#include "NBSPSC_Queue.hpp"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <thread>
+#include <unistd.h>
+
+const int MAX_ARRAY_CNT = 1000;
+const int MAX_TEST = 1000000 ;
+
+typedef struct __ST_QUEUE_DATA__
+{
+    int  data_index;
+    char data_record[4096];
+
+} ST_QUEUE_DATA ;
+
+
+//----------------------------------------------------------------------
+// TEST 
+//----------------------------------------------------------------------
+
+///////////////////////////////////////////////
+ThreadSafeQueueSPSC<ST_QUEUE_DATA, 1000> gQueue ;
+
+///////////////////////////////////////////////
+void Producer()
+{
+    ST_QUEUE_DATA queueData;
+    char szTemp[20];
+
+    for ( int i =0; i < MAX_TEST; i++)
+    {
+        queueData.data_index= i;
+        snprintf(szTemp, sizeof(szTemp), "data-%d", i );
+        memcpy(queueData.data_record, szTemp, strlen(szTemp));
+
+        while(1)
+        {
+            OPERATION_RESULT nRslt = gQueue.Push(&queueData);
+            if( nRslt != QUEUE_OK )
+            {
+                usleep(1);
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+///////////////////////////////////////////////
+void Consumer()
+{
+    size_t nVerify = 0;
+
+    ST_QUEUE_DATA* pQueueData;
+
+    for ( int i =0; i < MAX_TEST; i++)
+    {
+        while(1)
+        {
+            OPERATION_RESULT nRslt = gQueue.Pop(& pQueueData) ;
+            if( nRslt != QUEUE_OK )
+            {
+                usleep(1);
+                continue;
+            }
+            else
+            {
+                if( pQueueData->data_index != nVerify )
+                {
+                    printf("[%s-%d] Errorr %d-%zu [%s]\n", __func__, __LINE__, pQueueData->data_index, nVerify, pQueueData->data_record ); 
+                    exit(0);
+                }
+
+                nVerify++;
+
+                gQueue.CommitPop();
+
+                printf("[%s]  Pop: index=%d / %s\n", __func__,  i, pQueueData->data_record ); 
+
+                break;
+            }
+        }
+    }
+}
+
+///////////////////////////////////////////////
+int main ()
+{
+    std::thread ConsumerThread(Consumer);
+    std::thread ProducerThread(Producer);
+
+    ConsumerThread.join();
+    ProducerThread.join();
+
+    return 0;
+}
+
+// g++ -std=c++11 -pthread -o NBSPSC_QueueTest NBSPSC_QueueTest.cpp
+
